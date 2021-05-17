404520084dc5cb40aa1d61df1cd1d3f756b65779
SSARIpa3 <swati.0491@gmail.com>
2021-05-17 11:12:45 +0530

    Issue#4 : Create Localhost route Config for Beer Inventory service , closes #4

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -15,6 +15,8 @@ public class LocalHostRouteConfig {
                 .uri("http://localhost:8081"))
                 .route(r -> r.path("/api/v1/customers*","/api/v1/customers/*")
                 .uri("http://localhost:8082"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                .uri("http://localhost:8083"))
                 .build();
     }
 }
